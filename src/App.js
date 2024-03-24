716e5bbd85946b9f6d85d7062ebd28b3ba5b1307
VISHAL TOMAR <139646037+vishal0644@users.noreply.github.com>
2024-03-24 16:00:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Carousel from './Component/Carousel';
+import Heading from './Component/Heading';
+import HeroSection from './Component/HeroSection';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+      <HeroSection />
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
